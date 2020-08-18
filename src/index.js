9d43ed934386a22454b56d648a7bc75a06d52a2b
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-08-18 09:35:27 +0200

    Add reproduction for eslint-plugin-import too

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { min } from '@my-org/utils/math';
 
+// Just to test if import/no-unresolved works correctly
+import okayToImport from './okay-to-import';
+
+okayToImport();
+
 export default function main() {
   return min(3, 5, 7); // returns 3
 }
